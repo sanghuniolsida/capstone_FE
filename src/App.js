0fd6ef096bfe3748bf7a37d9fd6d2c66f9eb0e8d
KangJeungHun <128660252+KangJeungHun@users.noreply.github.com>
2024-11-09 21:30:28 +0900

    App.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Clothregister from './pages/Clothregister';
-import Mycloth from './pages/Mycloth';
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
-    <Router>
-      <div>
-        <h1>Moipzy</h1>
-        <Routes>
-          <Route path="/clothregister" element={<Clothregister />} />
-          <Route path="/mycloth" element={<Mycloth />} />
-        </Routes>
-      </div>
-    </Router>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
