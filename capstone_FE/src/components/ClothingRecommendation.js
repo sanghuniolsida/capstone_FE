691fc1925b1a2fa1d7d3ed1981a0785886b57f80
sanghun98 <p0526000@gmail.com>
2024-11-18 01:17:41 +0900

    여행 페이지 5일치 기상 예보 개발

--- capstone_FE/src/components/ClothingRecommendation.js
+++ capstone_FE/src/components/ClothingRecommendation.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './ClothingRecommendation.css';
 import { PiTShirtDuotone, PiPants } from "react-icons/pi";
+import { FaArrowUpRightFromSquare } from "react-icons/fa6";
 
 
 const ClothingRecommendation = ({ temperature }) => {
