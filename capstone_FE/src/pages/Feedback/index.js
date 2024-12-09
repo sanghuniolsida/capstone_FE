8e2b75283e83d8f45b859dd00a2412ca60bf877e
sanghun98 <p0526000@gmail.com>
2024-12-09 15:58:17 +0900

    여행 페이지 옷 추천 연동

--- capstone_FE/src/pages/Feedback/index.js
+++ capstone_FE/src/pages/Feedback/index.js
@@ -95,7 +95,7 @@ const Feedback = ({ onClose }) => {
                     value="SATISFIED"
                     onChange={(e) => setFeedback(e.target.value)}
                   />
-                  SATISFIED
+                  NORMAL
                 </label>
                 <label>
                   <input
